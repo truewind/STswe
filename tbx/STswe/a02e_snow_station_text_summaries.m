48906287db84c6003f3b837ea3418ba894532c80
truewind <mraleig1@gmail.com>
2024-01-12 14:42:04 -0700

    updated code to create super region csvs for SWE, transfer to NSIDC

--- tbx/STswe/a02e_snow_station_text_summaries.m
+++ tbx/STswe/a02e_snow_station_text_summaries.m
@@ -1,96 +1,148 @@
 clear; close all;
 
 
-%% loading
+for super_reg = 0:3
+    % where
+    %   super_reg = 0 is original, all sites
+    %   super_reg = 1 is western US (lower 48) only
+    %   super_reg = 2 is Alaska
+    %   super_reg = 3 is Canada
 
-%%% load settings
-snowtoday_settings;
+    %% loading
 
-%%% load the database
-load(all_database);
+    %%% load settings
+    snowtoday_settings;
 
-%%% load temp files w/ snow analysis
-load('temp_climSWE.mat')
-iYR2 = iYR; iMO2 = iMO; iDA2 = iDA;
+    %%% load the database
+    load(all_database);
 
-load('temp_dSWE.mat')
-if iYR~=iYR2 || iMO2~=iMO || iDA~=iDA2
-    disp('WARNING: Dates do not match up for dSWE and climSWE for the daily text summary')
-end
+    %%% load temp files w/ snow analysis
+    load('temp_climSWE.mat')
+    iYR2 = iYR; iMO2 = iMO; iDA2 = iDA;
 
-load('temp_percentPeakSWE.mat')
-if iYR~=iYR2 || iMO2~=iMO || iDA~=iDA2
-    disp('WARNING: Dates do not match up for percentPeakSWE and climSWE for the daily text summary')
-end
-
-%% prep
-
-%%% only keep sites in our domain
-keepSTA = nan.*dSWE;
-for j=1:numel(keepSTA)
-    if numel(char(SNOW.STA_HUC02{j}))>1 || numel(char(SNOW.STA_STATE{j}))>1
-        keepSTA(j)=1;
-    else
-        keepSTA(j)=0;
+    load('temp_dSWE.mat')
+    if iYR~=iYR2 || iMO2~=iMO || iDA~=iDA2
+        disp('WARNING: Dates do not match up for dSWE and climSWE for the daily text summary')
     end
-end
 
-keepSTA = find(keepSTA==1);
-Name = SNOW.STA_NAME(keepSTA);
-State = SNOW.STA_STATE(keepSTA);
-Lat = SNOW.STA_LAT(keepSTA);
-Lon = SNOW.STA_LON(keepSTA);
-Elev_m = SNOW.STA_ELEV_m(keepSTA);
-climSWE = SWE_pnrm(keepSTA);
-dSWE = dSWE(keepSTA);
-HUC02 = SNOW.STA_HUC02(keepSTA);
-HUC04 = SNOW.STA_HUC04(keepSTA);
-
-%% write file with all stations for this date
-
-YYYYMMDD = datestr(datenum(iYR,iMO,iDA), 'yyyymmdd');
-filepath_SWEsummary = fullfile(path_staging, ['SnowToday_USwest_' YYYYMMDD '_SWEsummary.txt']);
-fid = fopen(filepath_SWEsummary, 'w');
-
-
-%%% shifted header to the top
-fprintf(fid, '%s\n', ['SnowToday Calculated SWE Summary Data : ' datestr(datenum(iYR,iMO,iDA), 'yyyy-mm-dd')]);
-fprintf(fid, '%s\n', 'Column01 : site name');
-fprintf(fid, '%s\n', 'Column02 : latitude');
-fprintf(fid, '%s\n', 'Column03 : longitude');
-fprintf(fid, '%s\n', 'Column04 : elevation');
-fprintf(fid, '%s\n', ['Column05 : SWE (' units_figs_text ')']);
-fprintf(fid, '%s\n', 'Column06 : percent of median long-term (25+yr) SWE on this date');
-fprintf(fid, '%s\n', 'Column07 : percent of long-term (25+yr) peak SWE');
-fprintf(fid, '%s\n', ['Column08 : daily change in SWE (' units_figs_text ')']);
-fprintf(fid, '%s\n', 'Column09 : State');
-fprintf(fid, '%s\n', 'Column10 : HUC02');
-fprintf(fid, '%s\n', 'Column11 : HUC04');
-
-fprintf(fid,'%s\n', 'Name,Lat,Lon,Elev_m,SWE,normSWE,percentPeakSWE,dSWE,State,HUC02,HUC04');
-head_str = '%s,%.4f,%.4f,%.1f,%.1f,%.1f,%.1f,%.1f,%s,%s,%s\n';
-for j=1:numel(keepSTA)
-    %%% HUC02
-    if isempty(HUC02{j})==0
-        H2=char(HUC02(j));
-    else
-        H2='N/A';
+    load('temp_percentPeakSWE.mat')
+    if iYR~=iYR2 || iMO2~=iMO || iDA~=iDA2
+        disp('WARNING: Dates do not match up for percentPeakSWE and climSWE for the daily text summary')
     end
-    
-    %%% HUC04
-    if isempty(HUC04{j})==0
-        H4=char(HUC04(j));
-    else
-        H4='N/A';
+
+    %% prep
+
+    YYYYMMDD = datestr(datenum(iYR,iMO,iDA), 'yyyymmdd');
+
+    %%% only keep sites in our domain
+    keepSTA = nan.*dSWE;
+    if super_reg==0
+        super_filename=['SnowToday_USwest_' YYYYMMDD '_SWEsummary.txt'];
+        %%% all
+        for j=1:numel(keepSTA)
+            if numel(char(SNOW.STA_HUC02{j}))>1 || numel(char(SNOW.STA_STATE{j}))>1
+                keepSTA(j)=1;
+            else
+                keepSTA(j)=0;
+            end
+        end
+    elseif super_reg==1
+        super_filename= '26000.csv';
+        %%% western US lower 48
+        for j=1:numel(keepSTA)
+
+            if numel(char(SNOW.STA_HUC02{j}))>1 || numel(char(SNOW.STA_STATE{j}))>1
+                if strcmp(SNOW.STA_STATE{j}, 'USAK')==1 || strcmp(SNOW.STA_STATE{j}, 'CABC')==1
+                    keepSTA(j)=0;
+                else
+                    keepSTA(j)=1;
+                end
+            else
+                keepSTA(j)=0;
+            end
+
+        end
+    elseif super_reg==2
+        super_filename= '26100.csv';
+        %%% Alaska
+        for j=1:numel(keepSTA)
+            if strcmp(SNOW.STA_STATE{j}, 'USAK')==1
+                keepSTA(j)=1;
+            else
+                keepSTA(j)=0;
+            end
+        end
+    elseif super_reg==3
+        super_filename= '26101.csv';
+        %%% Canada BC
+        for j=1:numel(keepSTA)
+            if strcmp(SNOW.STA_STATE{j}, 'CABC')==1
+                keepSTA(j)=1;
+            else
+                keepSTA(j)=0;
+            end
+        end
     end
+
+    keepSTA = find(keepSTA==1);
+    Name = SNOW.STA_NAME(keepSTA);
+    State = SNOW.STA_STATE(keepSTA);
+    Lat = SNOW.STA_LAT(keepSTA);
+    Lon = SNOW.STA_LON(keepSTA);
+    Elev_m = SNOW.STA_ELEV_m(keepSTA);
+    climSWE = SWE_pnrm(keepSTA);
+    dSWE = dSWE(keepSTA);
+    HUC02 = SNOW.STA_HUC02(keepSTA);
+    HUC04 = SNOW.STA_HUC04(keepSTA);
+
+    %% write file with all stations for this date
+
     
+    filepath_SWEsummary = fullfile(path_staging, super_filename);
+    fid = fopen(filepath_SWEsummary, 'w');
+
     
-    fprintf(fid, head_str, char(Name(j)), Lat(j), Lon(j), Elev_m(j), SWEc(j), climSWE(j), SWE_percentPeakSWE(j), dSWE(j), char(State(j)), H2, H4);
+    %%% shifted header to the top
+    fprintf(fid, '%s\n', ['SnowToday Calculated SWE Summary Data : ' datestr(datenum(iYR,iMO,iDA), 'yyyy-mm-dd')]);
+    fprintf(fid, '%s\n', 'Column01 : site name');
+    fprintf(fid, '%s\n', 'Column02 : latitude');
+    fprintf(fid, '%s\n', 'Column03 : longitude');
+    fprintf(fid, '%s\n', 'Column04 : elevation');
+    fprintf(fid, '%s\n', ['Column05 : SWE (' units_figs_text ')']);
+    fprintf(fid, '%s\n', 'Column06 : percent of median long-term (25+yr) SWE on this date');
+    fprintf(fid, '%s\n', 'Column07 : percent of long-term (25+yr) peak SWE');
+    fprintf(fid, '%s\n', ['Column08 : daily change in SWE (' units_figs_text ')']);
+    fprintf(fid, '%s\n', 'Column09 : State');
+    fprintf(fid, '%s\n', 'Column10 : HUC02');
+    fprintf(fid, '%s\n', 'Column11 : HUC04');
+
+    fprintf(fid,'%s\n', 'Name,Lat,Lon,Elev_m,SWE,normSWE,percentPeakSWE,dSWE,State,HUC02,HUC04');
+    head_str = '%s,%.4f,%.4f,%.1f,%.1f,%.1f,%.1f,%.1f,%s,%s,%s\n';
+    for j=1:numel(keepSTA)
+        %%% HUC02
+        if isempty(HUC02{j})==0
+            H2=char(HUC02(j));
+        else
+            H2='N/A';
+        end
+
+        %%% HUC04
+        if isempty(HUC04{j})==0
+            H4=char(HUC04(j));
+        else
+            H4='N/A';
+        end
+
+
+        fprintf(fid, head_str, char(Name(j)), Lat(j), Lon(j), Elev_m(j), SWEc(j), climSWE(j), SWE_percentPeakSWE(j), dSWE(j), char(State(j)), H2, H4);
+    end
+
+    fclose(fid);
+
+    %%% for consistency, check if any "N/A" values and replace with "NaN"
+    find_and_replace(filepath_SWEsummary, 'N/A', 'NaN');
+
+    %%% clear all except super_reg loop variable
+    clearvars -except super_reg
+
 end
-
-fclose(fid);
-
-
-%%% for consistency, check if any "N/A" values and replace with "NaN"
-find_and_replace(filepath_SWEsummary, 'N/A', 'NaN');
- 
--- tbx/STswe/a05_snow_station_data_access.m
+++ tbx/STswe/a05_snow_station_data_access.m
@@ -12,7 +12,10 @@ snowtoday_settings;
 %%
 
 %%% look at files in the staging directory
-D = dir([fullfile(path_staging, '*_SWEsummary.txt')]);
+D1 = dir([fullfile(path_staging, '*_SWEsummary.txt')]);  % our original txt file 
+D2 = dir([fullfile(path_staging, '*.csv')]);             % new format for nsidc by super region
+D = [D1; D2];   % merge into single listing
+
 
 %%% keep track of number of files transferred to nusnow
 files_transferred = 0;
@@ -24,35 +27,50 @@ if isempty(D)==0
     for j=1:nfiles
         %%% get current filename
         iname = char(D(j).name);
-        
-        %%% get YYYYMMDD from filename (between 2nd and 3rd underscores)
-        a = find(iname=='_');
-        uscore2 = a(2);
-        uscore3 = a(3);
-        YYYYMMDD = iname(uscore2+1:uscore3-1);
-        [Y,M,~,~,~,~]=datevec(datenum(YYYYMMDD, 'yyyymmdd'));
-        if M>=10
-            iWY = Y+1;
+
+        if strcmp(iname(end-2:end), 'txt')==1
+            %%% original txt file format
+            flag_format = 1;
         else
-            iWY = Y;
+            %%%% new csv file format by super region
+            flag_format = 0;
         end
-        
-        %%% move to archive
-        path_dest = fullfile(path_PL_text_data, ['WY' num2str(iWY)], 'SWESummary');
-        if exist(path_dest,'dir')==7
-        else
-            mkdir(path_dest);
+
+        if flag_format==1
+            %%% get YYYYMMDD from filename (between 2nd and 3rd underscores)
+            a = find(iname=='_');
+            uscore2 = a(2);
+            uscore3 = a(3);
+            YYYYMMDD = iname(uscore2+1:uscore3-1);
+            [Y,M,~,~,~,~]=datevec(datenum(YYYYMMDD, 'yyyymmdd'));
+            if M>=10
+                iWY = Y+1;
+            else
+                iWY = Y;
+            end
+
+            %%% move to archive
+            path_dest = fullfile(path_PL_text_data, ['WY' num2str(iWY)], 'SWESummary');
+            if exist(path_dest,'dir')==7
+            else
+                mkdir(path_dest);
+            end
         end
-        
+
+
         try
             %%% transfer to nusnow via scp and using SSH key
             system(['scp -i ' char(path_ssh_key) ' ' fullfile(path_staging, iname) ' snow_today@nusnow.colorado.edu:' path_nusnow_swe]);
-        
+
             %%% add to tally of files transferred
             files_transferred = files_transferred + 1;
             
-            %%% archive to PL
-            movefile(fullfile(path_staging, iname), path_dest);
+            if flag_format==1
+                %%% archive to PL
+                movefile(fullfile(path_staging, iname), path_dest);
+            else
+                delete(fullfile(path_staging, iname))
+            end
         catch
             % if no folder setup, then issue warning and delete the png
             disp(['No destination setup at: ' path_dest])
